6eaf0b18d73f01f8a46a82d09f0c590c36f6667c
Daniel <dgkim@ucdavis.edu>
2024-01-08 11:04:22 -0800

    first commit

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,9 @@
 FROM openjdk:17.0.2-jdk-slim
 ARG JAR_FILE=target/*.jar
 COPY ${JAR_FILE} app.jar
-ENTRYPOINT ["sh", "-c", "java ${JAVA_OPTS} -jar /app.jar"]
+
+RUN mkdir -p /usr/local/newrelic
+ADD ./newrelic/newrelic.jar /usr/local/newrelic/newrelic.jar
+ADD ./newrelic/newrelic.yml /usr/local/newrelic/newrelic.yml
+
+ENTRYPOINT ["java", "-javaagent:/usr/local/newrelic/newrelic.jar", "-jar", "/app.jar"]
--- src/main/java/demo/kafka/consumer/KafkaDemoConsumer.java
+++ src/main/java/demo/kafka/consumer/KafkaDemoConsumer.java
@@ -22,9 +22,28 @@ public class KafkaDemoConsumer {
     public void listen(@Payload DemoInboundEvent event) {
         log.info("Received message - event: " + event);
         try {
+            processMessage(rec); // Call to process the message for tracing
             demoService.process(event.getNumberOfEvents());
         } catch (Exception e) {
             log.error("Error processing message: " + e.getMessage());
         }
+    }
+
+    @Trace(dispatcher = true)
+    private static void processMessage(ConsumerRecord<String, String> rec) {
+        Headers dtHeaders = ConcurrentHashMapHeaders.build(HeaderType.MESSAGE);
+        for (Header header : rec.headers()) {
+            String headerValue = new String(header.value(), StandardCharsets.UTF_8);
+            if (header.key().equals("newrelic")) {
+                dtHeaders.addHeader("newrelic", headerValue);
+            }
+            if (header.key().equals("traceparent")) {
+                dtHeaders.addHeader("traceparent", headerValue);
+            }
+            if (header.key().equals("tracestate")) {
+                dtHeaders.addHeader("tracestate", headerValue);
+            }
+        }
+        NewRelic.getAgent().getTransaction().acceptDistributedTraceHeaders(TransportType.Kafka, dtHeaders);
     }
 }
